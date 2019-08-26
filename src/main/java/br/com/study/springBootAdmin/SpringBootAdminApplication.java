524d7b866045ce3f7c656a42fa53b4c76f75654e
Da Silva <rlsl@gft.com>
2019-08-26 13:58:59 -0300

    initial commit

---
+++ src/main/java/br/com/study/springBootAdmin/SpringBootAdminApplication.java
@@ -0,0 +1,19 @@
+package br.com.study.springBootAdmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+
+}
